284b0272adad81e464d319b65fc28ac8f75d4b66
Dexter Wilkins <P2569728@my365.dmu.ac.uk>
2021-02-23 15:25:07 +0000

    Added validation to Forename, Lastname and email.

--- PrintingFrontOffice/ACustomer.aspx.cs
+++ PrintingFrontOffice/ACustomer.aspx.cs
@@ -17,12 +17,47 @@ public partial class ACustomer : System.Web.UI.Page
     {        
         //create a new instance of clsCustomer
         clsCustomer ACustomer = new clsCustomer();
-        //capture the customer no
-        //ACustomer.CustomerNo = txtCustomerNo.Text;
-        //store the customer in the session object
-        Session["ACustomer"] = ACustomer;                  
-        //redirect to the viewer page
-        Response.Redirect("CustomerViewer.aspx");
+        //capture the title
+        string Title = txtTitle.Text;
+        //capture the forename
+        string ForeName = txtForeName.Text;
+        //capture the LastName
+        string LastName = txtLastName.Text;
+        //capture the email
+        string Email = txtCustomerEmail.Text;
+        //capture the phone no
+        string PhoneNo = txtPhoneNo.Text;
+        //capture the date added
+        string DateAdded = txtDateAdded.Text;
+        //variable to store any error messages
+        string Error = "";
+        //validate the data
+        Error = ACustomer.Valid(Title, ForeName, LastName, Email, PhoneNo, DateAdded);
+        if (Error == "")
+        {
+            //capture the title
+            ACustomer.Title = Title;
+            //capture the forename
+            ACustomer.ForeName = ForeName;
+            //capture the last name
+            ACustomer.LastName = LastName;
+            //capture the email
+            ACustomer.Email = Email;
+            //capture the phone no
+            ACustomer.PhoneNo = Convert.ToInt32(PhoneNo);
+            //capture the date added
+            ACustomer.DateAdded = Convert.ToDateTime(DateAdded);
+            //store the customer in the session object
+            Session["ACustomer"] = ACustomer;
+            //redirect to the viewer oage
+            Response.Write("CustomerViewer.aspx");
+        }
+        else
+        {
+            //display the error message
+            lblError.Text = Error;
+        }
+
     }
 
     protected void btnFind_Click(object sender, EventArgs e)
