8ce09cf4c97c48f99b97de34024b88595992713c
Cameron Nicholls <P2527847@my365.dmu.ac.uk>
2021-02-24 18:56:18 +0000

    Added testing and wote the first test for the order section of code (clsOrder).

---
+++ Printing Testing/tstOrder.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PrintingClasses;
+
+namespace Printing_Testing
+{
+    [TestClass]
+    public class tstOrder
+    {
+        
+        [TestMethod]
+        public void InstanceOk()
+        {
+
+            clsOrder AnOrder = new clsOrder();
+
+            Assert.IsNotNull(AnOrder);
+        }
+
+
+    }
+}
