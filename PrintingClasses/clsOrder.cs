0f6bcba4dc748953e1217a9e459efc7c338812ba
Cameron Nicholls <P2527847@my365.dmu.ac.uk>
2021-03-01 19:06:41 +0000

    Adding the DeileryDate Test to the tstOrder.cs

--- PrintingClasses/clsOrder.cs
+++ PrintingClasses/clsOrder.cs
@@ -1,6 +1,10 @@
-﻿namespace PrintingClasses
+﻿using System;
+
+namespace PrintingClasses
 {
     public class clsOrder
     {
+        public bool Active { get; set; }
+        public DateTime DeliveryDate { get; set; }
     }
 }
