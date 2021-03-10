8984768f64eecd99fc4119e40fe5821f651a7015
Joseph Jordan <P2536634@my365.dmu.ac.uk>
2021-03-10 02:31:34 +0000

    Added testing and class for stock management

---
+++ Printing Testing/tstStock.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PrintingClasses;
+
+namespace Printing_Testing
+{
+    [TestClass]
+    public class tstStock
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            clsStock stock = new clsStock();
+            Assert.IsNotNull(stock);
+        }
+    }
+}
