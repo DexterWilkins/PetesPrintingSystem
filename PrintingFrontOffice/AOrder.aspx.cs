3af06a15ae45518e29cd8300bd1d1c8ce7d2579d
Cameron Nicholls <P2527847@my365.dmu.ac.uk>
2021-03-24 20:23:22 +0000

    Cameron: Fixed Vaildation, broke the asp.net partNo

--- PrintingFrontOffice/AOrder.aspx.cs
+++ PrintingFrontOffice/AOrder.aspx.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PrintingClasses;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -12,25 +13,27 @@ public partial class AOrder : System.Web.UI.Page
 
     }
 
-    protected void btnFind_Click(object sender, EventArgs e)
+    protected void btnFind_Click1(object sender, EventArgs e)
     {
         //create an instance of the OrderClass
         clsOrder AnOrder = new clsOrder();
         //variable to store the primary key
         Int32 OrderNo;
+
+        Boolean Found = false;
         //variable to store the result of the Find
-        OrderNo = Convert.ToInt32(txtOrderNo.Text);
+        OrderNo = Convert.ToInt32( txtOrderNo.Text);
         //find the record
         Found = AnOrder.Find(OrderNo);
         //if found
         if (Found == true)
         {
             //display the values of the properties in the form
-            txtDeliveryDate.Text = AnOrder.DeliveryDate;
-            txtDateDelivered.Text = AnOrder.DateDelivered;
-            txtPartNo.Text = AnOrder.PartNo;
+            //txtDeliveryDate.Text = AnOrder.DeliveryDate.ToString;
+            //txtOrderDate.Text = AnOrder.OrderDate.ToString;
+            //txtPartNo.Text = AnOrder.PartNo;
         }
-
-
     }
+
+    
 }
