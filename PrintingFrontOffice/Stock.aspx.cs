bba7f38319359c0d3b0cee3f0b12d6324b4fe0d1
Joseph Jordan <P2536634@my365.dmu.ac.uk>
2021-03-25 18:50:36 +0000

    Creating the find method (Presentation layer)

--- PrintingFrontOffice/Stock.aspx.cs
+++ PrintingFrontOffice/Stock.aspx.cs
@@ -6,6 +6,7 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
+
 public partial class Stock : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
@@ -23,5 +24,28 @@ public partial class Stock : System.Web.UI.Page
         Session["stock"] = stock;
         //redirect to the viewer page
         Response.Redirect("StockViewer.aspx");
+    }
+
+    protected void btnFind_Click(object sender, EventArgs e)
+    {
+        //create a new instance of clsStock
+        clsStock stock = new clsStock();
+        //variable to store the primary key
+        Int32 StockNo;
+        //variable to store the result of the find operation
+        Boolean Found = false;
+        //get the primary key entered by the user
+        StockNo = Convert.ToInt32(txtStockNo);
+        //find the record
+        Found = stock.Find(StockNo);
+        //if found
+        if (Found == true)
+        {
+            //display the values of the properties in the form
+            txtStockNo.Text = stock.StockNo;
+            txtDescription.Text = stock.StockDescription;
+            txtLocation.Text = stock.StockLocation;
+            txtDateAdded.Text = stock.DateAdded.ToString();
+        }
     }
 }
