87f6d05ce77e3dcfe9725db5b39b5376393f429c
Joseph Jordan <P2536634@my365.dmu.ac.uk>
2021-03-26 06:55:16 +0000

    Added collection class

---
+++ Printing Testing/tstStockCollection.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PrintingClasses;
+using System.Collections.Generic;
+
+namespace Printing_Testing
+{
+    [TestClass]
+    public class tstStockCollection
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            //create an instance of the class i want to create
+            clsStockCollection AllStock = new clsStockCollection();
+            //test to see that it exists
+            Assert.IsNotNull(AllStock);
+        }
+
+        [TestMethod]
+        public void StockListOK()
+        {
+            //create an instance of the class I want to create
+            clsStockCollection AllStock = new clsStockCollection();
+            //create some test data to assign to the property
+            List<clsStock> TestList = new List<clsStock>();
+            //add an item to the list
+            clsStock TestItem = new clsStock();
+            //set properties
+            TestItem.InStock = true;
+            TestItem.StockNo = 2;
+            TestItem.DateAdded = DateTime.Now.Date;
+            TestItem.StockDescription = "Part";
+            TestItem.StockLocation = "Warehouse";
+            //add the item to the list
+            AllStock.StockList = TestList;
+            //test to see that the two values are the same
+            Assert.AreEqual(AllStock.StockList, TestList);
+        }
+
+        [TestMethod]
+        public void ThisStockPropertyOK()
+        {
+            //create an instance of thte class I want to create 
+            clsStockCollection AllStock = new clsStockCollection();
+            //create some test data to assign to the property
+            clsStock TestStock = new clsStock();
+            //set the properties of the test object
+            TestStock.InStock = true;
+            TestStock.StockNo = 2;
+            TestStock.DateAdded = DateTime.Now.Date;
+            TestStock.StockDescription = "Part";
+            TestStock.StockLocation = "Warehouse";
+            //assign the data to the property
+            AllStock.ThisStock = TestStock;
+            //test to see that the two values are the same
+            Assert.AreEqual(AllStock.ThisStock, TestStock);
+        }
+
+        [TestMethod]
+        public void ListAndCountOK()
+        {
+            //create an instance of the class I want to create
+            clsStockCollection AllStock = new clsStockCollection();
+            //create some test data to assign to the property
+            List<clsStock> TestList = new List<clsStock>();
+            //add an item to the list
+            clsStock TestItem = new clsStock();
+            //set it's properties
+            TestItem.InStock = true;
+            TestItem.StockNo = 2;
+            TestItem.DateAdded = DateTime.Now.Date;
+            TestItem.StockDescription = "Part";
+            TestItem.StockLocation = "Warehouse";
+            //add the item to the test list
+            TestList.Add(TestItem);
+            //assign the data to the property
+            AllStock.StockList = TestList;
+            //test to see that the two values are the same
+            Assert.AreEqual(AllStock.Count, TestList.Count);
+        }
+
+        //constructor for the class
+        
+    }
+}
---
+++ PrintingClasses/clsStockCollection.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PrintingClasses
+{
+    public class clsStockCollection
+    {
+        private List<clsStock> mStockList = new List<clsStock>();
+        public List<clsStock> StockList
+        {
+            get
+            {
+                //return the private data
+                return mStockList;
+            }
+            set
+            {
+                //set the private data
+                mStockList = value;
+            }
+        }
+        public int Count
+        {
+            get
+            {
+                //return the private data
+                return mStockList.Count;
+            }
+            set
+            {
+                //
+            }
+        }
+        public clsStock ThisStock { get; set; }
+
+        public clsStockCollection()
+        {
+            //var for the index
+            Int32 Index = 0;
+            //var to store the record count
+            Int32 RecordCount = 0;
+            //object for the dataconnection
+            clsDataConnection DB = new clsDataConnection();
+            //execute the stored procedure
+            DB.Execute("sproc_tblStock_SelectAll");
+            //get the count of records
+            RecordCount = DB.Count;
+            //while there are records to process
+            while (Index < RecordCount)
+            {
+                //create a blank stock
+                clsStock stock = new clsStock();
+                //read in the fields from the current record
+                stock.InStock = Convert.ToBoolean(DB.DataTable.Rows[Index]["InStock"]);
+                stock.StockNo = Convert.ToInt32(DB.DataTable.Rows[Index]["StockNo"]);
+                stock.DateAdded = Convert.ToDateTime(DB.DataTable.Rows[Index]["DateAdded"]);
+                stock.StockDescription = Convert.ToString(DB.DataTable.Rows[Index]["StockDescription"]);
+                stock.StockLocation = Convert.ToString(DB.DataTable.Rows[Index]["StockLocation"]);
+                //add the record to the private data member
+                mStockList.Add(stock);
+                //point at the next record
+                Index++;
+            }
+        }
+    }
+}
